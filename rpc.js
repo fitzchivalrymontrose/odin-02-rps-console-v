fc0e5aefa1d1a0e9e96043d7cf86ec1a158dcf39
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2021-12-14 22:41:09 -0500

    layout basic function names

---
+++ rpc.js
@@ -0,0 +1,37 @@
+const choices = ['rock', 'paper', 'scissors'];
+
+
+
+function compareSelections (player, computer) {
+// rock beats scissors
+// scissors beats paper
+// paper beats rock
+}
+
+function computerPlay () {
+// call computerSelection()
+// return result
+}
+
+function playerSelection () {
+// prompt player for choice
+// if input is valid, return choice
+// else give error message and re-prompt player
+}
+
+function computerSelection () {
+// choose one of three options at random
+}
+
+
+function playRound (playerSelection, computerSelection) {
+// get player selection ... prompt player(call playerSelection)
+    const playerSelection = ;
+// get computer selection ... call computerSelection()
+// compare player and computer selections
+// based on rules, determine and display winner
+}
+
+function game () {
+// playRound 5 times
+}
