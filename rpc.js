3a3ac373089ef250e8aafcf912a38cc57b2e3742
fitzchivalrymontrose <fitzchivalrymontrose@protonmail.com>
2021-12-15 03:23:09 -0500

    coded playerSelection function

--- rpc.js
+++ rpc.js
@@ -9,29 +9,55 @@ function compareSelections (player, computer) {
 }
 
 function computerPlay () {
-// call computerSelection()
+// randomly choose between three options
 // return result
 }
 
 function playerSelection () {
 // prompt player for choice
+    let choice = prompt('Rock, Paper, or Scissors? Choose: ').toLowerCase();
+
 // if input is valid, return choice
+    switch (choice) {
+        case 'rock': 
+            choice = 'rock';
+            break;
+        case 'paper': 
+            choice = 'paper';
+            break;
+        case 'scissors': 
+            choice = 'scissors';
+            break;
+        default:
+            return playerSelection();
+    }
+    return choice;
+
 // else give error message and re-prompt player
 }
 
-function computerSelection () {
-// choose one of three options at random
-}
-
-
 function playRound (playerSelection, computerSelection) {
 // get player selection ... prompt player(call playerSelection)
-    const playerSelection = ;
 // get computer selection ... call computerSelection()
-// compare player and computer selections
+// compare playconsole.log(randomNumber(1, 3));er and computer selections
 // based on rules, determine and display winner
 }
 
 function game () {
 // playRound 5 times
 }
+
+function randomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + 1);
+}
+
+console.log(randomNumber(1, 3));
+console.log(randomNumber(1, 3));
+console.log(randomNumber(1, 3));
+console.log(randomNumber(1, 3));
+console.log(randomNumber(1, 3));
+console.log(randomNumber(1, 3));
+console.log(randomNumber(1, 3));
+console.log(randomNumber(1, 3));
+let playerChoice = playerSelection();
+console.log(playerChoice);
